a95159d9a57c59d08a47c772aacf547aa41963af
grburgess <jmichaelburgess@gmail.com>
2022-07-29 16:16:38 +0200

    clear init

--- gammapy_plugin/__init__.py
+++ gammapy_plugin/__init__.py
@@ -2,11 +2,14 @@
 
 """Top-level package for Gammapy Plugin."""
 
-__author__ = """J. Michael Burgess"""
-__email__ = 'jburgess@mpe.mpg.de'
+# please replace my name as this is boiler plate from
+# my cookie cutter
+
+# __author__ = """J. Michael Burgess"""
+# __email__ = 'jburgess@mpe.mpg.de'
 
 
-from .utils.logging import gammapy_plugin_config, show_configuration
+# from .utils.logging import gammapy_plugin_config, show_configuration
 
 from ._version import get_versions
 __version__ = get_versions()['version']

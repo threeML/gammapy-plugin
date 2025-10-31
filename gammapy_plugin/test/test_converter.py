609f0dd862f6600afef8e040d20baadb05bdeb76
Tobias Preis <tobias.preis@uibk.ac.at>
2025-10-31 14:17:11 +0100

    test for mapping

--- gammapy_plugin/test/test_converter.py
+++ gammapy_plugin/test/test_converter.py
@@ -1,4 +1,7 @@
+import numpy as np
+import astropy.units as u
 from astromodels.core.model import Model
+from astromodels.core.spectral_component import SpectralComponent
 from astromodels.functions import Powerlaw
 from astromodels.sources import PointSource
 
@@ -12,3 +15,51 @@ def test_source_converter():
     conv = AstromodelConverter(model, frame="galactic")
     sc = conv._converted_sources["test_ps"]
     assert sc.astromodels_source == ps
+
+
+def test_parameter_mapping():
+    pl1 = Powerlaw(index=-9.9, K=12)
+    pl2 = Powerlaw()
+    sc1 = SpectralComponent("comp1", pl1)
+    sc2 = SpectralComponent("comp2", pl2)
+    ps1 = PointSource(ra=0, dec=0, components=[sc1, sc2], source_name="test_ps")
+    ps2 = PointSource(
+        ra=10, dec=10, spectral_shape=Powerlaw(index=0, K=1), source_name="source2"
+    )
+    model = Model(*[ps1, ps2])
+    conv = AstromodelConverter(model, frame="galactic")
+    pl2.K.value = 100
+    conv._update_parameters()
+    assert (
+        conv._converted_sources["test_ps"]
+        .astromodels_source.components["comp2"]
+        .shape.K.value
+        == 100
+    )
+    pl1.index.value = 3.1415
+    assert (
+        conv._converted_sources["test_ps"]
+        .astromodels_source.components["comp1"]
+        .shape.index.value
+        == 3.1415
+    )
+    assert (
+        conv._converted_sources[
+            "source2"
+        ].astromodels_source.spectrum.main.shape.index.value
+        == 0
+    )
+
+
+def test_multi_comp():
+    pl1 = Powerlaw(index=-1, K=1)
+    pl2 = Powerlaw(index=-2, K=2)
+    sc1 = SpectralComponent("comp1", pl1)
+    sc2 = SpectralComponent("comp2", pl2)
+    ps1 = PointSource(ra=0, dec=0, components=[sc1, sc2], source_name="test_ps")
+    model = Model(ps1)
+    conv = AstromodelConverter(model, frame="galactic")
+    assert np.isclose(
+        ps1(1),
+        conv.gammapy_models[0].spectral_model(1 * u.keV).to("keV-1cm-2s-1").value,
+    )

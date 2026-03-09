927454da5ef2168d39ed3a10f1e5bd71a2937a9b
Tobias Preis <tobias.preis@uibk.ac.at>
2026-03-09 11:00:17 +0100

    fix: exlucde md_docs from sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ extensions = [
 ]
 
 templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "md_docs"]
 
 
 # -- Options for HTML output -------------------------------------------------

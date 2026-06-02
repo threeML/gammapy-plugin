5a80d8eb299f991e943e4170af5d7a42a1fd77f5
Tobias Preis <tobias.preis@uibk.ac.at>
2026-06-02 19:08:25 +0200

    refactor: subpackage for models

---
+++ gammapy_plugin/models/__init__.py
@@ -0,0 +1,10 @@
+from .spatial import PointSourceModelConverted, SpatialModelConverted
+from .spectral import SpectralModelConverted
+from .temporal import TemporalModelConverted
+
+__all__ = [
+    "SpectralModelConverted",
+    "PointSourceModelConverted",
+    "SpatialModelConverted",
+    "TemporalModelConverted",
+]

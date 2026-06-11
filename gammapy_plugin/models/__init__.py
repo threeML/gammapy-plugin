465554d59b0c3544ed4c788e98ab0015376d30b5
Tobias Preis <tobias.preis@uibk.ac.at>
2026-06-11 13:49:00 +0200

    add: remove temporal model - gets handled directly in astromodels

--- gammapy_plugin/models/__init__.py
+++ gammapy_plugin/models/__init__.py
@@ -1,10 +1,8 @@
 from .spatial import PointSourceModelConverted, SpatialModelConverted
 from .spectral import SpectralModelConverted
-from .temporal import TemporalModelConverted
 
 __all__ = [
     "SpectralModelConverted",
     "PointSourceModelConverted",
     "SpatialModelConverted",
-    "TemporalModelConverted",
 ]

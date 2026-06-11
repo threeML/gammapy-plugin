090217036247fa3673ddc1d87d7ff73f03456527
Tobias Preis <tobias.preis@uibk.ac.at>
2026-06-11 10:17:46 +0200

    build: switch from versioneer to setuptools_scm for versioning

--- gammapy_plugin/__init__.py
+++ gammapy_plugin/__init__.py
@@ -1,10 +1,5 @@
 """Top-level package for Gammapy Plugin."""
 
-from ._version import get_versions
 from .gammapy_like import GammapyLike
-
-__version__ = get_versions()["version"]
-del get_versions
-
 
 __all__ = ["GammapyLike"]

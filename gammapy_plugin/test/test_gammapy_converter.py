9e5e5779a3ef9c9e3b86bf1fee8f76e7a9e577f3
Tobias Preis <tobias.preis@uibk.ac.at>
2026-08-03 09:30:49 +0200

    chore: ruff

--- gammapy_plugin/test/test_gammapy_converter.py
+++ gammapy_plugin/test/test_gammapy_converter.py
@@ -1,8 +1,3 @@
-from astromodels.functions import Disk_on_sphere, Powerlaw
-from astromodels.sources import ExtendedSource, PointSource
-
-from gammapy_plugin.converter import GammapyConverter
-
 # TODO: implement
 
 
